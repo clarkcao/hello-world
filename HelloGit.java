f3e9db9c06ef32d5354ba97084feeb31f687c975
caolipeng <caolipeng@iboxpay.com>
2015-02-13 16:44:27 +0800

    Signed-off-by: caolipeng <caolipeng@iboxpay.com>.新增Java文件到远程git

---
+++ HelloGit.java
@@ -0,0 +1,12 @@
+package com.clark;
+
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+		System.out.println(addMethod(2, 5));
+	}
+	
+	private static int addMethod(int a,int b){
+		return a+b;
+	}
+}
